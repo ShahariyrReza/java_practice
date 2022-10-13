5041c3016cb75dd36630af37bca8c80076128dbc
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-10-13 18:40:47 +0600

    Update get_user_input.java

--- get_user_input.java
+++ get_user_input.java
@@ -16,7 +16,7 @@ public class get_user_input {
          System.out.println("Enter your name :");
          
          String name = sc.nextLine();
-         System.out.println("Your value is: "+num);
-         System.out.println("Your name is :"+name);
+         System.out.println("Your value is "+num);
+         System.out.println("Your name is "+name);
     }
 }
