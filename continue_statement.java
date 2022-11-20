d3217b7d3b35be777a6b7d3349fac1ae123cd1e8
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-11-20 19:05:02 +0600

    Update continue_statement.java

--- continue_statement.java
+++ continue_statement.java
@@ -9,7 +9,7 @@ public class continue_statement {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             if (i==5 || i==7) {
-                continue;
+                continue; // 5 and 7 will not print...
             }
             System.out.println(i);
         }
