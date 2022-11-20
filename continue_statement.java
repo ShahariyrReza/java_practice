a4d787090b47ffcd1a30fa43fabb6db0755035da
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-11-20 19:04:15 +0600

    Add files via upload

---
+++ continue_statement.java
@@ -0,0 +1,17 @@
+
+package mycompany.myproject1;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class continue_statement {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i==5 || i==7) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
