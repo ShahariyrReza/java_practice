6142535b68171d51b187251c641ed248ac0d05be
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-11-20 18:54:47 +0600

    Add files via upload

---
+++ break_statement.java
@@ -0,0 +1,17 @@
+
+package mycompany.myproject1;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class break_statement {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i=i+2) {
+            if (i==12) {
+                break; //while the condition satisfiyed ,then break statement works.
+            }
+            System.out.println(i);
+        }
+    }
+}
