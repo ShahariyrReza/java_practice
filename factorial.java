1573620b44bae3f53fe208ece5acc16f5daf3df1
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-11-27 13:35:12 +0600

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -10,7 +10,7 @@ public class factorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num,fact=1; // fact er man 1 theke suru hobe..
-        System.out.println("Enter any number :");
+        System.out.println("Enter number for factorial  :");
         num=input.nextInt();
         
         for (int i = num; i>= 1; i--) { // 1,1 kore kombe input value er man..
